f3cbeb3618196d5f77c4245ac681e85e606980d2
YuraPaisanidi <yura.paisanidi@gmail.com>
2021-07-20 16:46:41 +0300

    quiz logic

--- js/app.js
+++ js/app.js
@@ -304,5 +304,253 @@ document.addEventListener("DOMContentLoaded", function() {
 		};
 		forms('.form');
 
+
+//--------------------------------Quiz--------------------------
+	const DATA = [
+		{
+			question: `Вы согласны регулярно приезжать в онкоцентр (каждые 2-3 недели) 
+								для обследования и лечения амбулаторно (в некоторых случаях приезжать для 
+								обследования необходимо 2-3 дня подряд).
+								`,
+			answers: [
+				{
+					id: 1,
+					value: 'Да',
+					correct: true,
+				},
+				{
+					id: 2,
+					value: 'Нет',
+					correct: false,
+				},
+			]
+		},
+
+		{
+			question: `Вам требуется проводить в постели дневное время для дополнительного отдыха?`,
+			answers: [
+				{
+					id: 3,
+					value: 'Да',
+					correct: true,
+				},
+				{
+					id: 4,
+					value: 'Нет',
+					correct: true,
+				},
+			]
+		},
+
+		{
+			question: `Вы проводите в постели больше половины дневного времени для отдыха?`,
+			answers: [
+				{
+					id: 5,
+					value: 'Да',
+					correct: false,
+				},
+				{
+					id: 6,
+					value: 'Нет',
+					correct: true,
+				},
+			]
+		},
+
+		{
+			question: `У вас есть хронические заболевания?`,
+			answers: [
+				{
+					id: 7,
+					value: 'Да',
+					correct: true,
+				},
+				{
+					id: 8,
+					value: 'Нет',
+					correct: true,
+				},
+			]
+		},
+
+		{
+			question: `У вас есть хронические заболевания, включая гипертоническую болезнь и/или 
+									ишемическую болезнь сердца, и/или аритмию, и/или сахарный диабет (или другие) и вы 
+									каждый день принимаете препараты и регулярно контролируете артериальное давление и/или уровень глюкозы?
+									`,
+			answers: [
+				{
+					id: 9,
+					value: 'Да',
+					correct: true,
+				},
+				{
+					id: 10,
+					value: 'Нет',
+					correct: false,
+				},
+			]
+		},
+
+		{
+			question: `За последние 6 мес вы переносили инфаркт или инсульт?`,
+			answers: [
+				{
+					id: 11,
+					value: 'Да',
+					correct: false,
+				},
+				{
+					id: 12,
+					value: 'Нет',
+					correct: true,
+				},
+			]
+		},
+
+		{
+			question: `Подготовительный период перед лечением в клиническое исследовании (скрининг) 
+								может занять от 7 до 28 дней (иногда больше). Вы согласны отложить начало лечения, 
+								чтобы доктора могли выяснить подходит ли Вам исследование (если окажется, 
+								что исследование Вам не подходит, это время будет потрачено напрасно)? 
+				`,
+			answers: [
+				{
+					id: 13,
+					value: 'Да',
+					correct: true,
+				},
+				{
+					id: 14,
+					value: 'Нет',
+					correct: false,
+				},
+			]
+		}
+	];
+
+	const localResults = {};
+
+	const quiz = document.getElementById('quiz');
+	const questions = document.getElementById('questions');
+	const indicator = document.getElementById('indicator');
+	const results = document.getElementById('results');
+	const btnNext = document.getElementById('btn-next');
+	const btnRestart = document.getElementById('btn-restart');
+
+	const renderQuestions = (index) => {
+		renderIndicator(index + 1);
+
+		questions.dataset.currentStep = index;
+
+		const renderAnswers = () => DATA[index].answers
+			.map((answer) => `
+				<li>
+					<label>
+						<input  class="answer-input" type="radio" name="${index}" value="${answer.id}">
+						${answer.value}
+					</label>
+				</li>
+			`)
+			.join('');
+
+		questions.innerHTML = `
+			<div class="quiz-questions__item">
+					<div class="quiz-questions__item_question"><h3>${DATA[index].question}</h3></div>
+					<ul class="quiz-questions__item_answers">
+						${renderAnswers()}
+					</ul>
+				</div>
+		`;
+	};
+
+	const renderResults = () => {
+		let content = '';
+		if(localResults[0] === '2' || localResults[2] === '5' || localResults[4] === '10' || localResults[5] === '11' || localResults[6] === '14') {
+			content += `
+						<div class="quiz-results__item">
+							<div class="quiz-results__item_question">Скорее всего клинические исследования Вам не подходят</div>
+						</div>
+			`;
+		} else {
+			content += `
+						<div class="quiz-results__item">
+							<div class="quiz-results__item_question">Вам стоит попробовать принять участие в клиническом исследовании</div>
+						</div>
+			`;
+		}
+
+		// const getAnswers = (questionIndex) => DATA[questionIndex].answers
+		// .map((answer) => `<li>${answer.value}</li>`)
+		// .join('');
+
+		// DATA.forEach((question, index) => {
+		// 	content += `
+		// 		<div class="quiz-results__item">
+		// 			<div class="quiz-results__item_question">11111${question.question}</div>
+		// 			<ul class="quiz-results__item_answers">${getAnswers(index)}</ul>
+		// 		</div>
+			
+		// 	`;
+		// });
+
+		results.innerHTML = content;
+	};
+
+	const renderIndicator = (currentStep) => {
+		indicator.innerHTML = `${currentStep}/${DATA.length}`;
+	};
+
+	quiz.addEventListener('change', (event) => {
+		if(event.target.classList.contains('answer-input')) {
+			localResults[event.target.name] = event.target.value;
+			btnNext.disabled = false;
+
+		}
+		
+	});
+
+	quiz.addEventListener('click', (event) => {
+		if(event.target.classList.contains('btn-next')) {
+			const nextQuestionIndex = Number(questions.dataset.currentStep) + 1;
+
+			if(DATA.length === nextQuestionIndex) {
+				//go to results
+				questions.classList.add('questions-hidden');
+				indicator.classList.add('indicator-hidden');
+				results.classList.add('results-visible');
+				btnNext.classList.add('btn-next-hidden');
+				btnRestart.classList.add('btn-restart-visible');
+
+				renderResults();
+			}
+
+			if (localResults[1] === '4' || localResults[3] === '8'){
+					renderQuestions(nextQuestionIndex + 1);
+			} else {
+				//go to next question
+				renderQuestions(nextQuestionIndex);
+			}
+
+			btnNext.disabled = true;
+		}
+
+		if(event.target.classList.contains('btn-restart')) {
+			localResults = {};
+			results.innerHTML = '';
+
+			questions.classList.remove('questions-hidden');
+			indicator.classList.remove('indicator-hidden');
+			results.classList.remove('results-visible');
+			btnNext.classList.remove('btn-next-hidden');
+			btnRestart.classList.remove('btn-restart-visible');
+
+			// renderQuestions(0);
+
+		}
+	});
+
+	renderQuestions(0);
+
 });
-	
